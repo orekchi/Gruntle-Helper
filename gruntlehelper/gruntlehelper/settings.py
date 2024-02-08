3d42e3693555496774ce84cc720ea84e7187c177
orekchi <orekchi@gmail.com>
2024-02-08 15:32:01 +0200

    second commit, all works fine, search field too, upgrading to ckeditor 5

--- gruntlehelper/gruntlehelper/settings.py
+++ gruntlehelper/gruntlehelper/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'pythonapp',
     'pygmentify',
     'ckeditor',
+    'taggit',
 
 ]
 
--- gruntlehelper/kivyapp/views.py
+++ gruntlehelper/kivyapp/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Article
+from django.db.models import Q
+
 
 # Create your views here.
 
-def index(requests):
-    all_articles = Article.objects.all()
-    return render(requests, 'kivyapp/index.html', {'all_articles': all_articles})
+def index(request):
+    if 'search' in request.GET:
+        print(request.GET)
+        search = request.GET['search']
+        articles = Article.objects.filter(title__icontains=search)
+    else:
+        articles = Article.objects.all()
+    return render(request, 'kivyapp/index.html', {'articles': articles})
+
+
+
